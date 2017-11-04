b482795f628b9c2d5b35aa7152013784f8db17ce
shadid12 <shadidhaque2012@gmail.com>
2017-11-04 15:58:39 -0400

    added new fields

--- components/BarcodeScaner.js
+++ components/BarcodeScaner.js
@@ -41,20 +41,27 @@ export default class BarcodeScaner extends Component {
 
 	_handleBarCodeRead = ({ type, data }) => {
     	// alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    	console.log(data);
     	var ro4 = data.split('R04~');
     	var address = ro4[1].split('|')[0];
     	var ro7 = data.split('R07~')[1];
     	var postal = ro7.split('|')[0];
+    	var ro6 = data.split('R06~')[1];
+    	var city = ro6.split('|')[0];
+    	var so2  = data.split('S02~')[1];
+    	var pin = so2.split('|')[0];
     	Alert.alert(
-		  'Barcode Scanned',
-		  'Send Data to Database',
+		  `Barcode Scanned`,
+		  `Address: ${address} Postal Code: ${postal} City: ${city} Pin: ${pin}`,
 		  [
 		    {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
 		    {text: 'Send', onPress: () => {
 		    	axios.post('http://shadid12.herokuapp.com/item', {
 		    		username: this.props.name,
 		    		address:  address,
-		    		postal:  postal
+		    		postal:  postal,
+		    		city: city,
+		    		pin: pin
  		    	}).then((res) => {
  		    		console.log(res);
  		    	})
